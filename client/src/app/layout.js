dac9da409a3d1d88663e56cc5ed141aa13b281cf
Akbar Haleel <akbarhaleel508@gmail.com>
2024-11-08 01:57:10 +0530

    change to img

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,12 +2,12 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "https://i.postimg.cc/TYQghNSb/synx-up-01-Yb-N9-XRn8-LOi5-J9-Qb.png",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "https://i.postimg.cc/TYQghNSb/synx-up-01-Yb-N9-XRn8-LOi5-J9-Qb.png",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
